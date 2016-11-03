d062659cde9d0888216534b6c04608bbe2d208d6
Angele <angele.lys@hotmail.com>
2016-11-03 15:26:29 -0400

    omg

--- GameClient/src/ca/nicho/client/entity/EntityBattleship.java
+++ GameClient/src/ca/nicho/client/entity/EntityBattleship.java
@@ -5,7 +5,7 @@ import ca.nicho.client.SpriteSheet;
 
 public class EntityBattleship extends EntityPlayer {
 
-	private static Sprite[] sprites = {SpriteSheet.SPRITE_BATTLESHIP, SpriteSheet.SPRITE_BATTLESHIP};
+	private static Sprite[] sprites = {SpriteSheet.SPRITE_BATTLESHIP, SpriteSheet.SPRITE_MEDIC_1};
 	
 	public EntityBattleship(float x, float y, int id) {
 		super(x, y, sprites, id);
