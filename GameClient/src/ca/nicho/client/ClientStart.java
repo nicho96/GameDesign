4bd007adc4d950e252681d9cf5abd5661b63aecd
Angele <angele.lys@hotmail.com>
2016-12-04 19:20:02 -0500

    SampleText

--- GameClient/src/ca/nicho/client/ClientStart.java
+++ GameClient/src/ca/nicho/client/ClientStart.java
@@ -409,13 +409,10 @@ public class ClientStart extends JFrame {
 				drawGUISprite(this.getWidth()/2 - SpriteSheet.SPRITE_SHIPCRAFT.width/2, SpriteSheet.SPRITE_BACKGROUND_TOP.height/2 - SpriteSheet.SPRITE_SHIPCRAFT.height/2 - 10, SpriteSheet.SPRITE_SHIPCRAFT);
 				drawGUISprite(mapX, mapY, SpriteSheet.SPRITE_MAP_SMALL);
 				
-				
 				Sprite countMissiles = new Sprite(store.missiles);
-				drawGUISprite(FRAME_WIDTH - SpriteSheet.SPRITE_SLOT.width - 15, 5, SpriteSheet.SPRITE_SLOT_NUM);
-				drawGUISprite(FRAME_WIDTH - (SpriteSheet.SPRITE_SLOT.width/2 - SpriteSheet.SPRITE_AIRSTRIKE.width/2) - 15 - countMissiles.width, (SpriteSheet.SPRITE_SLOT.height/2 - SpriteSheet.SPRITE_AIRSTRIKE.height/2) + 5, SpriteSheet.SPRITE_AIRSTRIKE);
-				drawGUISprite(FRAME_WIDTH - (SpriteSheet.SPRITE_SLOT_NUM.width/2) - (countMissiles.width/2) - 14, SpriteSheet.SPRITE_SLOT_NUM.height - countMissiles.height - 2, countMissiles);
-				
-				
+				drawGUISprite(FRAME_WIDTH - SpriteSheet.SPRITE_SLOT_NUM.width - 15, 5, SpriteSheet.SPRITE_SLOT_NUM);
+				drawGUISprite(FRAME_WIDTH - (SpriteSheet.SPRITE_SLOT_NUM.width/2 - SpriteSheet.SPRITE_AIRSTRIKE.width/2) - 15 - countMissiles.width, (SpriteSheet.SPRITE_SLOT.height/2 - SpriteSheet.SPRITE_AIRSTRIKE.height/2) + 5, SpriteSheet.SPRITE_AIRSTRIKE);
+				drawGUISprite(FRAME_WIDTH - (SpriteSheet.SPRITE_SLOT_NUM.width/2 - countMissiles.width/2) - 14, SpriteSheet.SPRITE_SLOT_NUM.height - countMissiles.height - 2, countMissiles);
 				
 				//Draw the ships on the map
 				drawGUISprite(15, 15, SpriteSheet.SPRITE_SHIPS_BACKGROUND);	
